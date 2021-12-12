c66457e52f6d6acff5cc4e62dbe895ab64344810
changjandamtw <changjandamtw@gmail.com>
2021-12-12 13:25:48 +0800

    demo-logic-done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import './App.css';
+import react, { useState } from 'react';
+
+import style from './App.module.css';
 
 function App() {
+  const [firstDemoText, setFirstDemoText] = useState(false);
+  const [secondDemoText, setSecondDemoText] = useState(true);
+
+  const firstBtnClickHandler = () => {
+    setFirstDemoText((preState) => !preState);
+  };
+
+  const secondBtnClickHandler = () => {
+    if (firstDemoText) {
+      setSecondDemoText((preState) => !preState);
+    }
+  };
+
   return (
-    <div className="App">
-      
+    <div className={style.main}>
+      <button className={style.btn} onClick={firstBtnClickHandler}>
+        {firstDemoText ? 'on' : 'off'}
+      </button>
+      <button className={style.btn} onClick={secondBtnClickHandler}>
+        {secondDemoText ? 'true' : 'false'}
+      </button>
     </div>
   );
 }
